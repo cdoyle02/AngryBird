955d12416827419039ec71f3de52e93975617243
Harrison Rodriguez <harrisonmrod@gmail.com>
2019-11-01 14:02:41 -0500

    first commit

---
+++ AngryBird/AngryBird/Models/AbQuestion.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace AngryBird.Models
+{
+    public class AbQuestion
+    {
+        public int QuestionID { get; set; }
+        public string Question { get; set; }
+        public int Rating { get; set; }
+    }
+}
---
+++ AngryBird/AngryBird/QuestionRepository.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace AngryBird
+{
+    public class QuestionRepository
+    {
+        
+    }
+}
